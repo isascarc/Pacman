274fba353328a48957cad97becc407f8ce748e9e
isascar <51045423+isascarc@users.noreply.github.com>
2021-12-16 15:04:43 +0200

    Add files via upload

---
+++ pacmen2/FrmSelectProfile.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace PacmanGame
+{
+    partial class FrmSelectProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmmdOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LbxProfiles = new System.Windows.Forms.ListBox();
+            this.CmdPassword = new System.Windows.Forms.Button();
+            this.CmdRename = new System.Windows.Forms.Button();
+            this.CmdDelete = new System.Windows.Forms.Button();
+            this.CmdNew = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmmdOk
+            // 
+            this.CmmdOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.CmmdOk.Location = new System.Drawing.Point(35, 340);
+            this.CmmdOk.Name = "CmmdOk";
+            this.CmmdOk.Size = new System.Drawing.Size(72, 23);
+            this.CmmdOk.TabIndex = 5;
+            this.CmmdOk.Text = "בחר";
+            this.CmmdOk.UseVisualStyleBackColor = true;
+            this.CmmdOk.Click += new System.EventHandler(this.CmmdOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(35, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "בחר פרופיל מתוך הרשימה";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // LbxProfiles
+            // 
+            this.LbxProfiles.FormattingEnabled = true;
+            this.LbxProfiles.Location = new System.Drawing.Point(35, 108);
+            this.LbxProfiles.Name = "LbxProfiles";
+            this.LbxProfiles.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.LbxProfiles.Size = new System.Drawing.Size(282, 147);
+            this.LbxProfiles.TabIndex = 0;
+            // 
+            // CmdPassword
+            // 
+            this.CmdPassword.Location = new System.Drawing.Point(35, 262);
+            this.CmdPassword.Name = "CmdPassword";
+            this.CmdPassword.Size = new System.Drawing.Size(72, 23);
+            this.CmdPassword.TabIndex = 4;
+            this.CmdPassword.Text = "...סיסמא";
+            this.CmdPassword.UseVisualStyleBackColor = true;
+            this.CmdPassword.Click += new System.EventHandler(this.CmdPassword_Click);
+            // 
+            // CmdRename
+            // 
+            this.CmdRename.Location = new System.Drawing.Point(113, 262);
+            this.CmdRename.Name = "CmdRename";
+            this.CmdRename.Size = new System.Drawing.Size(62, 23);
+            this.CmdRename.TabIndex = 3;
+            this.CmdRename.Text = "שנה שם";
+            this.CmdRename.UseVisualStyleBackColor = true;
+            this.CmdRename.Click += new System.EventHandler(this.CmdRename_Click);
+            // 
+            // CmdDelete
+            // 
+            this.CmdDelete.Location = new System.Drawing.Point(181, 262);
+            this.CmdDelete.Name = "CmdDelete";
+            this.CmdDelete.Size = new System.Drawing.Size(70, 23);
+            this.CmdDelete.TabIndex = 2;
+            this.CmdDelete.Text = "מחיקה";
+            this.CmdDelete.UseVisualStyleBackColor = true;
+            this.CmdDelete.Click += new System.EventHandler(this.CmdDelete_Click);
+            // 
+            // CmdNew
+            // 
+            this.CmdNew.Location = new System.Drawing.Point(257, 262);
+            this.CmdNew.Name = "CmdNew";
+            this.CmdNew.Size = new System.Drawing.Size(60, 23);
+            this.CmdNew.TabIndex = 1;
+            this.CmdNew.Text = "הוספה";
+            this.CmdNew.UseVisualStyleBackColor = true;
+            this.CmdNew.Click += new System.EventHandler(this.CmdNew_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(35, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 37);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "בחירת פרופיל";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmSelectProfile
+            // 
+            this.AcceptButton = this.CmmdOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(362, 381);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CmdNew);
+            this.Controls.Add(this.CmdDelete);
+            this.Controls.Add(this.CmdRename);
+            this.Controls.Add(this.CmdPassword);
+            this.Controls.Add(this.LbxProfiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmmdOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmSelectProfile";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "בחירת פרופיל";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmmdOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox LbxProfiles;
+        private System.Windows.Forms.Button CmdPassword;
+        private System.Windows.Forms.Button CmdRename;
+        private System.Windows.Forms.Button CmdDelete;
+        private System.Windows.Forms.Button CmdNew;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ pacmen2/Waiter.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PacmanGame
+{
+    partial class Waiter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BorderColor = System.Drawing.Color.Red;
+            this.rectangleShape1.BorderWidth = 10;
+            this.rectangleShape1.Location = new System.Drawing.Point(5, 5);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(353, 207);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(363, 218);
+            this.shapeContainer1.TabIndex = 17;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Guttman Kav", 26F);
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(32, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 45);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "loading...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Pic1
+            // 
+            this.Pic1.Location = new System.Drawing.Point(32, 34);
+            this.Pic1.Name = "Pic1";
+            this.Pic1.Size = new System.Drawing.Size(300, 100);
+            this.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pic1.TabIndex = 19;
+            this.Pic1.TabStop = false;
+            this.Pic1.Paint += new System.Windows.Forms.PaintEventHandler(this.Pic1_Paint);
+            // 
+            // Waiter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.Pic1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Waiter";
+            this.Size = new System.Drawing.Size(363, 218);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Pic1;
+    }
+}
